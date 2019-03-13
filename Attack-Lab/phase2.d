e89d19faaf91019550c6d278902b8ea34f8a682a
Oceane Andreis <oceane.andreis@colorado.edu>
2019-03-12 20:28:21 -0600

    Attack-Lab
    Computer System Assignment
    Phase 1-5

---
+++ Attack-Lab/phase2.d
@@ -0,0 +1,10 @@
+
+phase2.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 af 87 f9 14 	mov    $0x14f987af,%rdi
+   7:	68 c8 12 40 00       	pushq  $0x4012c8
+   c:	c3                   	retq   
